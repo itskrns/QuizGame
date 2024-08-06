89d4f5cb8f55c784454b66172913169b67720120
itskrns <itskrns@gmail.com>
2024-08-06 14:54:19 +0530

    New Cateogries

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,5 @@
-export const API_URL = `https://opentdb.com/api.php?type=multiple&amount=10&`
+//export const API_URL = `https://opentdb.com/api.php?type=multiple&amount=10&`
+export const API_URL = `https://www.otriviata.com/api.php?amount=10&type=multiple&`
 export const SCORE_PER_ANS = 10
 export const DEFAULT_INDEX = 0
 export const TIMEOUT_SEC = 10
